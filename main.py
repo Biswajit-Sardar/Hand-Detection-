7083f9acf640406629ca6a5fa507eb4ccf8c5103
Biswajit Sardar <biswajitsardar04082005@gmail.com>
2025-11-28 20:28:26 +0530

    implement main loop for hand detection and image display

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ cap.set(3,1920)
 cap.set(4,1080)
 
 
+while True:
+    _, img=cap.read()  # Read the frame from the camera and store it in img , ( _,) means we are ignoring the first return value
+    img = cv2.flip(img, 1)
+    hand,img= detector.findHands(img) # Detect hands in the image
+    cv2.imshow("Image",img)  # Display the image in a window named "Image"
+    cv2.waitKey(1)  #wait 1 mili second
+    if cv2.waitKey(1)== ord("q"):  # If the 'q' key is pressed so remove the camera
+        break
